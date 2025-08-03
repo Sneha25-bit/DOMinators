8661b41840578a5e3f52bc4ab70cfe8a87c062f1
Vinay6783 <vahbvinay@gmail.com>
2025-08-03 10:15:08 +0530

    Basic Stripe

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,9 +9,13 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 from datetime import timedelta
+
+load_dotenv()
+STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
