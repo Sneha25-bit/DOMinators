4eb31bbf2aacb01e88b7e7cd1d8e38e7824406ab
Sunay Bhattacharjee <sunay2403@gmail.com>
2025-08-11 20:03:15 +0530

    Local run

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -115,7 +115,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=os.environ.get('DATABASE_URL',
+         f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
         conn_max_age=600,  
     )
 }
